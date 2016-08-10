046d194dcfb13a7196be041ea76521a6f4437a60
warnov <warnov@gmail.com>
2016-08-10 16:29:37 -0500

    Calling API code refined and extra-lines removed. Now showing an example on how to add the token to a get request in order to get som information from the api in a secure way.

--- WinFormsTester/Form1.cs
+++ WinFormsTester/Form1.cs
@@ -2,46 +2,29 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
 
 namespace WinFormsTester
 {
     public partial class Form1 : Form
     {
+        string _token = string.Empty;
+        string baseuri = "https://localaccountstest.azurewebsites.net/";
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnRequest_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            WebClient client = new WebClient();
-            client.UseDefaultCredentials = true;
             var user = txtUser.Text;
             var pwd = txtPwd.Text;
-            var uri = "https://localhost:44329/Token";
-            var baseuri = "https://localaccountstest.azurewebsites.net/";
-            var parameters = $"grant_type=password&username={user}&password={pwd}";
-            string result = string.Empty;
-
-
-            //using (WebClient wc = new WebClient())
-            //{
-            //    wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
-            //    result = wc.UploadString(uri, parameters);
-            //}
-
- 
 
             using (var hc = new HttpClient())
             {
-                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-
                 hc.BaseAddress = new Uri(baseuri);
                 var content = new FormUrlEncodedContent(new[]
                 {
@@ -50,11 +33,30 @@ namespace WinFormsTester
                     new KeyValuePair<string, string>("password",pwd)
                 });
                 var oResult = hc.PostAsync("/Token", content).Result;
-                result = oResult.Content.ReadAsStringAsync().Result;                
+                var sResult = oResult.Content.ReadAsStringAsync().Result;
+                rtbResponse.Text = sResult;
+                if (sResult.Contains("access_token"))
+                {
+                    var jResult = JObject.Parse(sResult);
+                    _token = jResult["access_token"].ToString();
+                    rtbResponse.Text = _token;
+                }
             }
+        }
 
-
-            rtbResponse.Text = result;
+        private void btnRequest_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(_token))
+            {
+                using (var hc = new HttpClient())
+                {
+                    hc.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+                    hc.BaseAddress = new Uri(baseuri);
+                    var sResult = hc.GetStringAsync("/api/values").Result;
+                    rtbResponse.Text = sResult;
+                }
+            }
+            else MessageBox.Show("User is not logged in");
         }
     }
 }
